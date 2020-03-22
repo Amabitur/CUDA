2aee5e774052388bfe90e8d856afe056ac9b6ae7
Amabitur <alena.sludnova@gmail.com>
2020-03-22 23:44:34 +0400

    Дополнение

--- BilinearInterpolation/bilinear-grayscale.py
+++ BilinearInterpolation/bilinear-grayscale.py
@@ -3,11 +3,11 @@ from pycuda import driver, compiler, gpuarray
 import numpy as np
 import cv2
 import timeit
-from utils import prepare_image, normalize_image, bilinear
+from utils import bilinear_gr
 
-IMG_PATH = './data/seal.jpg'
-image = cv2.imread(IMG_PATH)
-M1, N1, _ = image.shape
+IMG_PATH = './data/deer.jpeg'
+image = cv2.imread(IMG_PATH, cv2.IMREAD_GRAYSCALE)
+M1, N1 = image.shape
 M2 = int(2*M1)
 N2 = int(2*N1)
 
@@ -29,29 +29,28 @@ stop = driver.Event()
 print("Считаем на ГПУ...")
 start.record()
 
-prep_image = prepare_image(image)
+#prep_image = prepare_image(image)
 tex = mod.get_texref("tex")
 tex.set_filter_mode(driver.filter_mode.LINEAR)
 tex.set_address_mode(0, driver.address_mode.CLAMP)
 tex.set_address_mode(1, driver.address_mode.CLAMP)
-driver.matrix_to_texref(prep_image, tex, order="C")
+driver.matrix_to_texref(image.astype(np.uint32), tex, order="C")
 
 bilinear_interpolation_kernel(driver.Out(result), driver.In(x_out), driver.In(y_out), np.int32(M1), np.int32(N1), np.int32(M2), np.int32(N2), block=block, grid=grid, texrefs=[tex])
-big_image = normalize_image(result, image.shape[2])
 stop.record()
 stop.synchronize()
 gpu_time = stop.time_since(start)
 print("Время интерполяции на ГПУ: %.3f ms" % (gpu_time))
-cv2.imwrite("./data/big-gpu-seal.jpg", big_image.astype(np.uint8))
+cv2.imwrite("./data/big-gpu-gray-deer.jpeg", result.astype(np.uint8))
 
 #p_image = prepare_image(image)
 
 print("Считаем на ЦПУ...")
 start = timeit.default_timer()
-cpu_result = bilinear(image)
+cpu_result = bilinear_gr(image)
 cpu_time = timeit.default_timer() - start
 print("Время интерполяции на ЦПУ: %.3f ms" % (cpu_time * 1e3))
 
 #big_cpu_image = normalize_image(cpu_result, image.shape[2])
 
-cv2.imwrite("./data/big-cpu-seal.jpg", cpu_result.astype(np.uint8))
+cv2.imwrite("./data/big-cpu-gray-deer.jpeg", cpu_result.astype(np.uint8))
--- BilinearInterpolation/utils.py
+++ BilinearInterpolation/utils.py
@@ -14,7 +14,7 @@ def normalize_image(image, channels):
     rgba_image = np.zeros((image.shape[0], image.shape[1], channels), dtype=np.uint32)
     for x in range(rgba_image.shape[0]):
         for y in range(rgba_image.shape[1]):
-            output_x_y = image[x, y]
+            output_x_y = image[y, x]
             for ch in range(rgba_image.shape[2]):
                 rgba_image[x, y, rgba_image.shape[2] - ch - 1] = output_x_y % 256
                 output_x_y >>= 8
@@ -43,3 +43,26 @@ def bi_pixel(image, w, h):
         d = image[k,l][ch]/255
         out.append((a + b + c + d)*255*0.25)
     return out
+
+def bilinear_gr(image):
+    preres = np.zeros((image.shape[0]*2, image.shape[1]*2), dtype=np.uint32)
+    w = preres.shape[0]
+    h = preres.shape[1]
+    for i in range(w):
+        for j in range(h):
+            preres[i, j] = bi_pixel_gr(image, i, j)
+    return preres
+
+def bi_pixel_gr(image, w, h):
+    scale = 2
+    out = []
+    i = int(w/scale)
+    j = int(h/scale)
+    k = 0 if i + 1 >= image.shape[0] else i + 1
+    l = 0 if j + 1 >= image.shape[1] else j + 1
+    a = image[i, j]/255
+    b = image[k,j]/255
+    c = image[i,l]/255
+    d = image[k,l]/255
+    result = (a + b + c + d)*255*0.25
+    return result
